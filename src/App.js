2422161dc4738e06d467deca936c3d587f76c76c
gergodancs <dancs.gergely@gmail.com>
2022-01-14 18:07:08 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./components/Card";
+import Cardwithclass from "./components/Cardwclass";
 
 function App() {
+  const peoples = [
+    { id: 0, name: "gergo", details: "rendom valami" },
+    { id: 1, name: "laci", details: "random valami" },
+    { id: 2, name: "feri", details: "rondom valami" },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {peoples.map((people) => {
+        return (
+          <Card name={people.name} details={people.details} key={people.id} />
+        );
+      })}
+      {peoples.map((people) => {
+        return (
+          <Cardwithclass
+            name={people.name}
+            details={people.details}
+            key={people.id}
+          />
+        );
+      })}
     </div>
   );
 }
